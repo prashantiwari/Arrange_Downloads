fe88f21d9246fd31d44baaac8d6fe6b91aa8f9ed
prashantiwari <tiwariprashant@outlook.com>
2016-08-25 04:21:09 +0530

    Create ToDatedFolder.py

---
+++ ToDatedFolder.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+from datetime import datetime
+from time import sleep
+todaystring = str(datetime.now())
+todaystring = todaystring[:10]
+lis=[]
+i=1
+destinationDir='C:\Users\Prashant Tiwari\Downloads\Temp\DownloadedOn'
+destinationDir = destinationDir + todaystring
+
+while os.path.exists(destinationDir):
+	destinationDir=destinationDir+str(i)
+	i+=1
+os.makedirs(destinationDir)
+lis=os.listdir('C:\Users\Prashant Tiwari\Downloads\Temp')
+print lis
+for x in lis:
+	if x==__file__:
+		continue
+	shutil.move(x, destinationDir)
+
+destinationpython = destinationDir + '\manager.py'
+print destinationpython
+
+sleep(5)
+
+shutil.copy('C:\Users\Prashant Tiwari\Downloads\Temp\manager.py','C:\Users\Prashant Tiwari\Downloads\Temp')
